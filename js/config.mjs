93cbe243401de81b05372e35ab88b3bfbeaf0cc4
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2025-07-18 09:32:31 +0200

    Beheer: gebruik nieuwe organisation-config setup (#118)
    Part of Logius-standaarden/Automatisering#51

---
+++ js/config.mjs
@@ -0,0 +1,102 @@
+import { loadRespecWithConfiguration } from "https://logius-standaarden.github.io/publicatie/respec/organisation-config.mjs";
+
+loadRespecWithConfiguration({
+    authors: [{
+        "company": "Logius",
+        "companyURL": "https://www.logius.nl/",
+        "name": "Jaron Azaria"
+    },
+    {
+        "company": "Kadaster",
+        "companyURL": "https://www.kadaster.nl/",
+        "name": "Martin Borgman"
+    },
+    {
+        "company": "Kennisnet",
+        "companyURL": "https://www.kennisnet.nl/",
+        "name": "Marc Fleischeuers"
+    },
+    {
+        "company": "Logius",
+        "companyURL": "https://www.logius.nl/",
+        "name": "Alexander Green"
+    },
+    {
+        "company": "Logius",
+        "companyURL": "https://www.logius.nl/",
+        "name": "Peter Haasnoot"
+    },
+    {
+        "company": "Logius",
+        "companyURL": "https://www.logius.nl/",
+        "name": "Heiko Hudig"
+    },
+    {
+        "company": "Logius",
+        "companyURL": "https://www.logius.nl/",
+        "name": "Martin van der Plas"
+    },
+    {
+        "company": "Logius",
+        "companyURL": "https://www.logius.nl/",
+        "name": "Stas Mironov"
+    },
+    {
+        "company": "Logius",
+        "companyURL": "https://www.logius.nl/",
+        "name": "Leon van der Ree"
+    },
+    {
+        "company": "RvIG",
+        "companyURL": "https://www.rvig.nl/",
+        "name": "Bob te Riele"
+    },
+    {
+        "company": "Logius",
+        "companyURL": "https://www.logius.nl/",
+        "name": "Remco Schaar"
+    },
+    {
+        "company": "Geonovum",
+        "companyURL": "https://www.geonovum.nl/",
+        "name": "Frank Terpstra"
+    },
+    {
+        "company": "Rijkswaterstaat",
+        "companyURL": "https://www.rijkswaterstaat.nl/",
+        "name": "Jan Jaap Zoutendijk"
+    }
+    ],
+    editors: [{
+        "company": "Geonovum",
+        "companyURL": "https://www.geonovum.nl/",
+        "name": "Frank Terpstra"
+    },
+    {
+        "company": "Geonovum",
+        "companyURL": "https://www.geonovum.nl/",
+        "name": "Jan van Gelder"
+    },
+    {
+        "company": "Logius",
+        "companyURL": "https://www.logius.nl/",
+        "name": "Alexander Green"
+    },
+    {
+        "company": "Logius",
+        "companyURL": "https://www.logius.nl/",
+        "name": "Martin van der Plas"
+    }
+    ],
+    github: "https://github.com/Logius-standaarden/OAuth-NL-profiel/",
+    issueBase: "https://github.com/Logius-standaarden/OAuth-NL-profiel/issues",
+    licence: "cc-by-nd",
+    previousPublishDate: "2024-12-03",
+    previousPublishVersion: "v1.1.0",
+    pubDomain: "api",
+    publishDate: "2025-01-09",
+    publishVersion: "1.2.0",
+    shortName: "oauth",
+    specStatus: "WV",
+    specType: "ST",
+});
