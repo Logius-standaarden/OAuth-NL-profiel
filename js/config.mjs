6a89cfea5f65acb678746d8780ded701717a4698
Alexander Green <alexander.green@logius.nl>
2026-08-13 13:58:05 +0200

    Beheer: editors, date, and status (#117)

--- js/config.mjs
+++ js/config.mjs
@@ -68,25 +68,11 @@ loadRespecWithConfiguration({
     }
     ],
     editors: [{
-        "company": "Geonovum",
-        "companyURL": "https://www.geonovum.nl/",
-        "name": "Frank Terpstra"
-    },
-    {
-        "company": "Geonovum",
-        "companyURL": "https://www.geonovum.nl/",
-        "name": "Jan van Gelder"
-    },
-    {
-        "company": "Logius",
-        "companyURL": "https://www.logius.nl",
-        "name": "Alexander Green"
-    },
-    {
-        "company": "Logius",
-        "companyURL": "https://www.logius.nl",
-        "name": "Martin van der Plas"
-    }
+            "company": "Logius",
+            "companyURL": "https://www.logius.nl",
+            "mailto:": "api@logius.nl",
+            "name": "Logius Standaarden"
+        },
     ],
     github: "https://github.com/Logius-standaarden/OAuth-NL-profiel/",
     issueBase: "https://github.com/Logius-standaarden/OAuth-NL-profiel/issues",
@@ -94,9 +80,9 @@ loadRespecWithConfiguration({
     previousPublishDate: "2024-12-03",
     previousPublishVersion: "v1.1.0",
     pubDomain: "api",
-    publishDate: "2025-01-09",
+    publishDate: "2025-07-18",
     publishVersion: "1.2.0",
     shortName: "oauth",
-    specStatus: "WV",
+    specStatus: "CV",
     specType: "ST",
 });
